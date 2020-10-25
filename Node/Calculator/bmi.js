eb03e0dde4412467be4fd0d2a8715e9879b9619d
iamharshgoel <harshgoel0012@gmail.com>
2020-10-26 00:11:38 +0530

    BMI Node File

---
+++ Node/Calculator/bmi.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/" ,function(request, response){
+  response.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/", function(request, response){
+  var weight = Number(request.body.weight);
+  var height = Number(request.body.height);
+
+  var bmi = weight/(height*height);
+
+  response.send("Your BMI is "+ bmi);
+})
+
+
+app.listen(3000, function(){
+  console.log("Server is hosted on port 3000");
+})
